42a820ac18754f67c3e81702d4ddaf745b28617b
Node <8974108+qwenode@users.noreply.github.com>
2022-08-01 11:37:44 +0800

    fork from hightman/xunsearch

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'qwenode/php-xunsearch',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'qwenode/php-xunsearch' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
